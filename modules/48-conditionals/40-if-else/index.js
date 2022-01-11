9edbb25a6c28c95a59897c5b2994bb7cc5200568
Mira <43781679+Waterlass@users.noreply.github.com>
2022-01-11 22:53:45 +0300

    Update index.js
    Решение без slice() и дополнительных переменных.

--- modules/48-conditionals/40-if-else/index.js
+++ modules/48-conditionals/40-if-else/index.js
@@ -1,12 +1,13 @@
 // BEGIN
-const buildUrl = (url, domain) => {
-  let normalizedUrl;
-  if (url.startsWith('/')) {
-    normalizedUrl = url.slice(1);
-  } else {
-    normalizedUrl = url;
+const buildUrl = (path, domain) => {
+
+  if (path.startsWith('/')) {
+    return (`https://${domain}${path}`);
   }
-  return `https://${domain}/${normalizedUrl}`;
+// работает и при отсутствии path
+  else {
+    return (`https://${domain}/${path}`);
+  }
 };
 // END
 
